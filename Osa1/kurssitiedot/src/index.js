3cb5c8cc0c8c6b8e8d4b652efa0e97743a8eb656
itsnicholas <nicholas.danford@gmail.com>
2020-06-14 13:59:33 +0300

    kurssitiedot step3

--- Osa1/kurssitiedot/src/index.js
+++ Osa1/kurssitiedot/src/index.js
@@ -39,18 +39,24 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header name={course} />
-      <Content name1={part1} number1={exercises1}  name2={part2} number2={exercises2}  name3={part3} number3={exercises3}/>
-      <Total sum={exercises1 + exercises2 + exercises3} />
+      <Content name1={part1.name} number1={part1.exercises}  name2={part2.name} number2={part2.exercises}  name3={part3.name} number3={part3.exercises}/>
+      <Total sum={part1.exercises + part2.exercises + part2.exercises} />
     </div>
   )
 }
