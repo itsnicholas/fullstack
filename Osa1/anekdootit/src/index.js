3c086c161c457337879f83edcaedb089aa135799
itsnicholas <nicholas.danford@gmail.com>
2020-06-14 19:27:27 +0300

    anekdootit step1

--- Osa1/anekdootit/src/index.js
+++ Osa1/anekdootit/src/index.js
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Button = (props) => {
+  return (
+    <button onClick={props.onClick}>
+      {props.name}
+    </button>
+  )
+}
+
 const App = (props) => {
-  const [selected, setSelected] = Math.floor(Math.random() * 6)
+  const [selected, setSelected] = useState(0)
 
   return (
     <div>
       {props.anecdotes[selected]}
-      <button onClick={() => setSelected}>
-      next anecdote
-      </button>
+      <br /><Button name="next anecdote" onClick={() => setSelected(Math.floor(Math.random() * 6))} />
     </div>
   )
 }
@@ -18,7 +24,7 @@ const anecdotes = [
   'If it hurts, do it more often',
   'Adding manpower to a late software project makes it later!',
   'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Any fool can write code that a computer can understand. Good pogrammers write code that humans can understand.',
   'Premature optimization is the root of all evil.',
   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
 ]
