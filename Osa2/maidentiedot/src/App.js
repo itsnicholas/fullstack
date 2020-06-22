a389502d5348840a973f739216463d51b60e673e
itsnicholas <nicholas.danford@gmail.com>
2020-06-22 17:11:30 +0300

    2.14*: maiden tiedot, step3

--- Osa2/maidentiedot/src/App.js
+++ Osa2/maidentiedot/src/App.js
@@ -25,6 +25,20 @@ const Countries  = (props) => {
 }
 
 const OneCountry  = ( {country} ) => {
+  const [weather, setWeather] = useState({location:{}, current:{}})
+  const apiKey = process.env.REACT_APP_API_KEY
+  const url = `http://api.weatherstack.com/current?access_key=${apiKey}&query=${country.capital}`
+  
+  useEffect(() => {
+    console.log('effect 2')
+    axios
+      .get(url)
+      .then(response => {
+        console.log('promise fulfilled 2')
+        setWeather(response.data)
+      })
+  }, [])
+  
   return (
     <div>
       <h1>{country.name}</h1>
@@ -38,7 +52,11 @@ const OneCountry  = ( {country} ) => {
           </li>
       ))}
       </ul>
-      <img src={country.flag} alt="flag" height="30%" width="30%" />
+      <img src={country.flag} alt="flag" height="20%" width="20%" />
+      <h2>Weather in {country.capital}</h2>
+      <b>temperature:</b> {weather.current.temperature} Celsius
+      <br /><img alt="weather icon" src={weather.current.weather_icons} />
+      <br /><b>wind:</b> {weather.current.wind_speed} mph direction {weather.current.wind_dir}
     </div>
   )
 }
@@ -54,18 +72,19 @@ const Country  = ( {country} ) => {
 }
 
 const App = () => {
-    const [ countries, setCountries] = useState([]) 
+    const [ countries, setCountries] = useState([])
     const [ newSearch, setNewSearch] = useState('')
-  
+
     useEffect(() => {
       console.log('effect')
       axios
         .get('https://restcountries.eu/rest/v2/all')
         .then(response => {
+          console.log('promise fulfilled')
           setCountries(response.data)
         })
     }, [])
-  
+
     const handleSearchChange = (event) => {
       console.log(event.target.value)
       setNewSearch(event.target.value)
