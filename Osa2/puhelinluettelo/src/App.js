6a412471ec955c0c824581386f049e75a92436a4
itsnicholas <nicholas.danford@gmail.com>
2020-06-23 14:39:02 +0300

    2.15: puhelinluettelo step7

--- Osa2/puhelinluettelo/src/App.js
+++ Osa2/puhelinluettelo/src/App.js
@@ -75,9 +75,14 @@ const App = () => {
         name: newName,
         number: newNumber
       }
-      setPersons(persons.concat(nameObject))
-      setNewName('')
-      setNewNumber('')
+
+      axios
+      .post('http://localhost:3001/persons', nameObject)
+      .then(response => {
+        setPersons(persons.concat(response.data))
+        setNewName('')
+        setNewNumber('')
+      }) 
     }  
   }
 
