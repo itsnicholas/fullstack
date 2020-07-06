24f76c08ce3b97a4f090282e77f096eb6aeb085f
itsnicholas <nicholas.danford@gmail.com>
2020-07-06 23:07:32 +0300

    4.4: apufunktioita ja yksikkötestejä, step2

--- Osa4/blogilista/utils/list_helper.js
+++ Osa4/blogilista/utils/list_helper.js
@@ -2,6 +2,17 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs2) => {
+  if (blogs2 && blogs2.length > 0) {
+    return blogs2
+    .map(blog => blog.likes)
+    .reduce((accumulator, curr) => accumulator + curr)
+  } else {
+    return 0
+  }
+}
+
 module.exports = {
-  dummy
+  dummy, 
+  totalLikes
 }
