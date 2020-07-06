bab5bbff8b37ce1594d7b1f547504f866de0e3c6
itsnicholas <nicholas.danford@gmail.com>
2020-07-06 16:04:26 +0300

    4.2 blogilista, step2 with middleware and error handling

--- Osa4/blogilista/app.js
+++ Osa4/blogilista/app.js
@@ -5,9 +5,30 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+const mongoose = require('mongoose')
+
+mongoose.set('useFindAndModify', false)
+mongoose.set('useCreateIndex', true)
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connection to MongoDB:', error.message)
+  })
+
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
+
 app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
--- Osa4/blogilista/models/blog.js
+++ Osa4/blogilista/models/blog.js
@@ -1,16 +1,5 @@
 const mongoose = require('mongoose')
-const config = require('../utils/config')
-const logger = require('../utils/logger')
 const uniqueValidator = require('mongoose-unique-validator')
-
-mongoose.set('useFindAndModify', false)
-mongoose.set('useCreateIndex', true)
-
-const mongoUrl = config.MONGODB_URI
-
-logger.info('connecting to', mongoUrl)
-
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true})
 
 const blogSchema = mongoose.Schema({
   title: String,
