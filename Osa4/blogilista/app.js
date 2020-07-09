a21c883ac37b19da5a208beb587842fed732bb17
itsnicholas <nicholas.danford@gmail.com>
2020-07-09 22:02:45 +0300

    4.20*: bloglist expansion, step8

--- Osa4/blogilista/app.js
+++ Osa4/blogilista/app.js
@@ -28,9 +28,10 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
 app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
+app.use(middleware.tokenExtractor)
 
-app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/blogs', blogsRouter)
 app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
