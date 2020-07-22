3563b0595d9ead31e33b70fc34ce497bc970acb8
itsnicholas <nicholas.danford@gmail.com>
2020-07-22 19:34:45 +0300

    6.12* paremmat anekdootit, step10

--- Osa6/redux-anecdotes/src/components/AnecdoteList.js
+++ Osa6/redux-anecdotes/src/components/AnecdoteList.js
@@ -5,7 +5,10 @@ import { notificationChange } from '../reducers/notificationReducer'
 
 const AnecdoteList = () => {
   const anecdotes = useSelector(state => state.anecdotes)
-  const sortedAnecdotes = anecdotes.sort((a, b) => b.votes - a.votes)
+  const filter = useSelector(state => state.filter)
+  const filteredAnecdotes = anecdotes.filter(anecdote => anecdote.content.toLowerCase().includes(filter.toLowerCase()))
+  const sortedAnecdotes = filteredAnecdotes.sort((a, b) => b.votes - a.votes)
+
   const dispatch = useDispatch()
 
   const vote = (anecdote) => {
