c393ed16507ad53ac4ca4902c22ca5c9336e87eb
itsnicholas <nicholas.danford@gmail.com>
2020-07-23 18:05:55 +0300

    6.15 anekdootit ja backend, step3

--- Osa6/redux-anecdotes/src/store.js
+++ Osa6/redux-anecdotes/src/store.js
@@ -1,4 +1,7 @@
-import { createStore, combineReducers } from 'redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
 
 import anecdoteReducer from './reducers/anecdoteReducer'
 import notificationReducer from './reducers/notificationReducer'
@@ -10,7 +13,11 @@ const reducer = combineReducers({
     filter: filterReducer
   })
 
-const store = createStore(reducer)
+const store = createStore(reducer,
+  composeWithDevTools(
+    applyMiddleware(thunk)
+  )
+  )
 
 
 export default store
