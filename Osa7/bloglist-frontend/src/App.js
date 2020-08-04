0c62bc57563bd7029108e33b20f67863639f468c
itsnicholas <nicholas.danford@gmail.com>
2020-08-04 15:43:23 +0300

    7.13: käyttäjien näkymä

--- Osa7/bloglist-frontend/src/App.js
+++ Osa7/bloglist-frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Blog from './components/Blog'
 import Notification from './components/Notification'
 import Togglable from './components/Togglable'
@@ -7,37 +7,71 @@ import { notificationChange, loginNotificationChange } from './reducers/notifica
 import { initializeBlogs } from './reducers/blogReducer'
 import { logInUser, initializeUser, logOutUser } from './reducers/userReducer'
 import { useSelector, useDispatch } from 'react-redux'
+import usersService from './services/users'
 
-const App = () => {
-  const blogFormRef = React.createRef()
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom"
 
-  const dispatch = useDispatch()
+const User = ({ user }) => {
+  console.log(user.username, 'user.username in App.js')
+  console.log(user.blogs.length, 'user.blogs.length in App.js') 
+  return (
+    <tr key={user.id}>
+      <td>{user.username}</td>
+      <td>{user.blogs.length}</td>
+    </tr>
+  )
+}
 
-  useEffect(() => {
-    dispatch(initializeBlogs())
-  },[dispatch])
+const Users = ({ users }) => {
+  console.log(users, 'users in App.js')
 
-  useEffect(() => {
-    dispatch(initializeUser())
-  },[dispatch])
+  const byBlogs = (u1, u2) => u2.blogs.length - u1.blogs.length
 
-  const blogs = useSelector(state => state.blogs)
-  const user = useSelector(state => state.user)
+  return (
+    <div>
+      <h2>Users</h2>
+      <table id='users'>
+        <tbody>
+          {users.sort(byBlogs).map(user =>
+            <User key={user.id} user={user} />
+          )}
+        </tbody>
+      </table>
+    </div>
+  )
+}
 
-  const handleLogin = async (event) => {
-    event.preventDefault()
-    dispatch(logInUser(
-      event.target.username.value, 
-      event.target.password.value, 
-      notificationChange, 
-      loginNotificationChange
-    ))
-  }
+const Blogs = ({ blogs, blogFormRef, user }) => {
+  const byLikes = (b1, b2) => b2.likes - b1.likes
 
-  const handleLogout = () => {
-    dispatch(logOutUser())
-  }
+  return (
+    <div>
+      <Togglable buttonLabel='create new blog'  ref={blogFormRef}>
+        <NewBlog user={user}/>
+      </Togglable>
 
+      {blogs.sort(byLikes).map(blog =>
+        <Blog
+          key={blog.id}
+          blog={blog}
+          own={user.username===blog.user.username}
+        />
+      )}
+    </div>
+  )
+}
+
+const Home = ({
+  user,
+  handleLogin,
+  handleLogout,
+  blogFormRef,
+  blogs
+}) => {
   if ( !user ) {
     return (
       <div>
@@ -60,8 +94,6 @@ const App = () => {
     )
   }
 
-  const byLikes = (b1, b2) => b2.likes - b1.likes
-
   return (
     <div>
       <h2>blogs</h2>
@@ -71,18 +103,68 @@ const App = () => {
       <p>
         {user.username} logged in <button onClick={handleLogout}>logout</button>
       </p>
+    </div>
+  )
+}
 
-      <Togglable buttonLabel='create new blog'  ref={blogFormRef}>
-        <NewBlog user={user}/>
-      </Togglable>
+const App = () => {
+  const blogFormRef = React.createRef()
+  const [users, setUsers] = useState([])
 
-      {blogs.sort(byLikes).map(blog =>
-        <Blog
-          key={blog.id}
-          blog={blog}
-          own={user.username===blog.user.username}
+  useEffect(() => {
+    usersService.getAll().then(users =>
+      setUsers(users)
+    )
+  },[])
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(initializeBlogs())
+  },[dispatch])
+
+  useEffect(() => {
+    dispatch(initializeUser())
+  },[dispatch])
+
+  const blogs = useSelector(state => state.blogs)
+  const user = useSelector(state => state.user)
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    dispatch(logInUser(
+      event.target.username.value,
+      event.target.password.value,
+      notificationChange, 
+      loginNotificationChange
+    ))
+  }
+
+  const handleLogout = () => {
+    dispatch(logOutUser())
+  }
+
+  return (
+    <div>
+      <Router>
+        <Home 
+          user={user} 
+          handleLogin={handleLogin}
+          handleLogout={handleLogout} 
+          blogFormRef={blogFormRef} 
+          blogs={blogs} 
         />
-      )}
+        <Switch>
+          <Route path="/users">
+            <Users users={users} />
+          </Route>
+          <Route path="/">
+            <Blogs blogs={blogs} 
+              blogFormRef={blogFormRef} 
+              user={user} />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   )
 }
