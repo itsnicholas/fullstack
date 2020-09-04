70bdc98c08e71705cfdec24d7edacb005cd97b98
itsnicholas <nicholas.danford@gmail.com>
2020-09-04 20:01:33 +0300

    9.3 Command line

--- Osa9/Bodymassindex/bmiCalculator.ts
+++ Osa9/Bodymassindex/bmiCalculator.ts
@@ -1,30 +1,53 @@
-type Result = string;
+interface MultiplyValues {
+  value1: number;
+  value2: number;
+}
 
-const calculateBmi = (a: number, b: number) : Result => {
-  console.log(Math.pow(a, 2), 'Math.pow(a, 2)')
-    if (b / Math.pow(a/100, 2)  <= 15) {
-    return 'Very severely underweight';
+const parseArguments2 = (args: Array<string>): MultiplyValues => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      value1: Number(args[2]),
+      value2: Number(args[3])
+    }
+  } else {
+    throw new Error('Provided values were not numbers!');
+  }
+}
+
+const calculateBmi = (a: number, b: number) => {
+  if (b / Math.pow(a/100, 2)  <= 15) {
+    console.log('Very severely underweight');
   } else if (b / Math.pow(a/100, 2) <= 16) {
-    return 'Severely underweight';
+    console.log('Severely underweight');
   } else if (b / Math.pow(a/100, 2) <= 18.5) {
-    return 'Underweight';
+    console.log('Underweight');
   } else if (b / Math.pow(a/100, 2) <= 25) {
-    return 'Normal (healthy weight)';
+    console.log('Normal (healthy weight)');
   } else if (b / Math.pow(a/100, 2) <= 30) {
-    return 'Overweight';
+    console.log('Overweight');
   } else if (b / Math.pow(a/100, 2) <= 35) {
-    return 'Obese Class I (Moderately obese)';
+    console.log('Obese Class I (Moderately obese)');
   } else if (b / Math.pow(a/100, 2) <= 40) {
-    return 'Obese Class II (Severely obese)';
+    console.log('Obese Class II (Severely obese)');
   } else if (b / Math.pow(a/100, 2) > 40) {
-    return 'Obese Class III (Very severely obese)';
+    console.log('Obese Class III (Very severely obese)');
   } else if (a <= 0 || b <= 0) {
-    return 'Can\'t calculate with 0 or less!';
+    console.log('Can\'t calculate with 0 or less!');
   }
 }
 
 try {
-  console.log(calculateBmi(180, 74))
+  const { 
+    value1, 
+    value2
+} = parseArguments2(process.argv);
+  calculateBmi(
+    value1, 
+    value2
+  );
 } catch (e) {
   console.log('Something went wrong, error message: ', e.message);
 }
