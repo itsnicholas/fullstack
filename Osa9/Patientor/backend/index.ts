55c640f5ee9c756cda705b492bd00fae03ca94e5
itsnicholas <nicholas.danford@gmail.com>
2020-09-06 14:11:20 +0300

    9.9: Patientor backend, step2

--- Osa9/Patientor/backend/index.ts
+++ Osa9/Patientor/backend/index.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 const app = express();
+import cors from 'cors';
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors());
 app.use(express.json());
 
-const PORT = 3000;
+const PORT = 3001;
 
 app.get('/ping', (_req, res) => {
   console.log('someone pinged here');
