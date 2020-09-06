3dbd1c1d360913c36e2e69a4eeedeea817bc8b91
itsnicholas <nicholas.danford@gmail.com>
2020-09-06 18:38:43 +0300

    9.10: Patientor backend, step3 (added some changes to be comitted)

--- Osa9/Patientor/backend/src/index.ts
+++ Osa9/Patientor/backend/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import diagnoseRouter from './routes/diagnoses';
 const app = express();
 import cors from 'cors';
 // eslint-disable-next-line @typescript-eslint/no-unsafe-call
@@ -11,6 +12,8 @@ app.get('/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
+
+app.use('/api/diagnoses', diagnoseRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
